502e9d444288174b27db027813850aa49fc280be
learn-zhezhe <126082301+learn-zhezhe@users.noreply.github.com>
2024-09-05 21:10:01 +0800

    Add files via upload

---
+++ Loss.py
@@ -0,0 +1,70 @@
+import torch
+from torch import nn
+import torch.nn.functional as F
+
+
+class DiceLoss(nn.Module):
+    """
+    Define the dice loss
+    """
+    def __init__(self, n_classes):
+        super(DiceLoss, self).__init__()
+        self.n_classes = n_classes
+
+    def _one_hot_encoder(self, input_tensor):
+        tensor_list = []
+        for i in range(self.n_classes):
+            temp_prob = input_tensor == i  # * torch.ones_like(input_tensor)
+            tensor_list.append(temp_prob.unsqueeze(1))
+        output_tensor = torch.cat(tensor_list, dim=1)
+        return output_tensor.float()
+
+    def _dice_loss(self, score, target):
+        target = target.float()
+        smooth = 1e-5
+        intersect = torch.sum(score * target)
+        y_sum = torch.sum(target * target)
+        z_sum = torch.sum(score * score)
+        loss = (2 * intersect + smooth) / (z_sum + y_sum + smooth)
+        loss = 1 - loss
+        return loss
+
+    def forward(self, inputs, target, weight=None, softmax=False):
+        if softmax:
+            inputs = torch.softmax(inputs, dim=1)#12, 6, 256, 256
+        target = self._one_hot_encoder(target)#[12, 6, 256, 256]
+        if weight is None:
+            weight = [1] * self.n_classes
+        assert inputs.size() == target.size(), 'predict {} & target {} shape do not match'.format(inputs.size(), target.size())
+        class_wise_dice = []
+        loss = 0.0
+        for i in range(0, self.n_classes):
+            dice = self._dice_loss(inputs[:, i], target[:, i])
+            class_wise_dice.append(1.0 - dice.item())
+            loss += dice * weight[i]
+        return loss / self.n_classes
+
+
+class FocalLoss(nn.Module):
+    """
+    Define the Focal Loss function
+    """
+    def __init__(self, alpha=0.25, gamma=2, reduction='mean'):
+        super(FocalLoss, self).__init__()
+        self.alpha = alpha
+        self.gamma = gamma
+        self.reduction = reduction
+
+    def forward(self, inputs, targets):
+        ce_loss = F.cross_entropy(inputs, targets, reduction=self.reduction)
+        pt = torch.exp(-ce_loss)
+        focal_loss = self.alpha * (1-pt)**self.gamma * ce_loss
+        return focal_loss
+
+
+def ce_loss(inputs, targets):
+    """
+    Define the cross-entropy loss function
+    """
+    return F.cross_entropy(inputs, targets, reduction='none').mean(1).mean(1)
+    # return F.binary_cross_entropy(F.sigmoid(inputs), targets, reduction='none').mean(1).mean(1)
